45d1532abcc96a5f75ce5db5850a1a5efc29fff8
«Oksana <oxana.shishkova@gmail.com>
2022-12-19 14:57:04 +0300

    Started committing

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// #4 На вход принимаем три числа, на выходе даем макс из них.
+
+Console.WriteLine("Enter number 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 3: ");
+int number3 = int.Parse(Console.ReadLine());
+int maximum = number1;
+
+if (maximum < number2) maximum = number2;
+if (maximum < number3) maximum = number3;
+
+Console.WriteLine($"Maximum = {maximum}");
